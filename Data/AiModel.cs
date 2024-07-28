28f8c9d7b47cbe3fc82664a6e293f9053c1272f3
gbenga.adaramodu <gbenga.adaramodu@cyberspace,net.ng>
2024-07-28 14:02:02 +0100

    pushed updateAdd project files.

---
+++ Data/AiModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounselBot.Data
+{
+    public  class AiModel
+    {
+        public List<Content> contents { get; set; }
+    }
+    //public class Content
+    //{
+    //    public List<Part> parts { get; set; }
+    //}
+
+    public class Part
+    {
+        public string text { get; set; }
+    }
+
+
+
+    //AI generated response
+
+    public class AiResponse
+    {
+        public List<Candidate> candidates { get; set; }
+        public UsageMetadata usageMetadata { get; set; }
+    }
+    public class Candidate
+    {
+        public Content content { get; set; }
+        public string finishReason { get; set; }
+        public int index { get; set; }
+        public List<SafetyRating> safetyRatings { get; set; }
+    }
+
+    public class Content
+    {
+        public List<Part> parts { get; set; }
+        public string role { get; set; }
+    }
+
+
+    public class SafetyRating
+    {
+        public string category { get; set; }
+        public string probability { get; set; }
+    }
+
+    public class UsageMetadata
+    {
+        public int promptTokenCount { get; set; }
+        public int candidatesTokenCount { get; set; }
+        public int totalTokenCount { get; set; }
+    }
+
+
+
+
+
+}
---
+++ Services/ChatMessage.cs
@@ -0,0 +1,114 @@
+﻿using CounselBot.Data;
+using SQLite;
+
+namespace ChatBot.Services
+{
+
+    public interface IChatMessage
+    {
+        Task<int> SaveChat(Chat chat);
+        Task<int> UpdateChat(Chat chat);
+        Task<int> DeleteChat(Chat chat);
+        Task<int> DeleteAllChat();
+        Task<List<Chat>> GetAllChats();
+        Task<Chat> GetChatById(int chatId);
+
+    }
+    public class ChatMessage : IChatMessage
+    {
+        private SQLiteAsyncConnection _context;
+        public ChatMessage(/*SQLiteAsyncConnection context*/)
+        {
+            //_context = context; 
+            SetUpDbAsync();
+        }
+
+        private async Task SetUpDbAsync()
+        {
+            if (_context == null)
+            {
+                var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "chat.db3");
+                _context = new SQLiteAsyncConnection(dbPath);
+                await _context.CreateTableAsync<Chat>();
+            }
+        }
+
+        public async Task<int> DeleteChat(Chat chat)
+        {
+            try
+            {
+                return await _context.DeleteAsync(chat);
+            }
+            catch(Exception ex){
+                return 0;    
+            }
+        }
+        public async Task<int> DeleteAllChat()
+        {
+            try
+            {
+                var chats = await _context.Table<Chat>().ToListAsync();
+                if(chats.Count > 0)
+                {
+                    foreach(var chat in chats)
+                    {
+                        await _context.DeleteAsync(chat);
+                    }
+                }
+                return 1;
+            }
+            catch(Exception ex){
+                return 0;    
+            }
+        }
+
+        public async Task<List<Chat>> GetAllChats()
+        {
+            try
+            {
+                return await _context.Table<Chat>().ToListAsync();
+            }
+            catch(Exception ex)
+            {
+                return new List<Chat>();    
+            }
+        }
+
+        public async Task<Chat> GetChatById(int chatId)
+        {
+            try
+            {
+                var chat = await _context.QueryAsync<Chat>($"SELECT * FROM {nameof(Chat)} WHERE Id ={chatId} ");
+
+                return chat.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                return new Chat();
+            }
+        }
+
+        public async Task<int> SaveChat(Chat chat)
+        {
+            try
+            {
+                return await _context.InsertAsync(chat);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+        public async Task<int> UpdateChat(Chat chat)
+        {
+            try
+            {
+                return await _context.UpdateAsync(chat);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
