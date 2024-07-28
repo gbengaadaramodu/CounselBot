489e081ccc3e42565a5c9d32f839056744ecdb42
gbenga.adaramodu <gbenga.adaramodu@cyberspace,net.ng>
2024-07-28 14:02:02 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing.Internal.PathSegments;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+namespace CounselBot
+{
+    public partial class App : Microsoft.Maui.Controls.Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            Current.On<Microsoft.Maui.Controls.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
+        }
+    }
+}
---
+++ Data/Chat.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounselBot.Data
+{
+    public class Chat
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string UniqueId { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
